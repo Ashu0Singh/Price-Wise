47800e02075dabbeca3e73f503248944aa6003d3
Ashu0Singh <ashusingh1417@gmail.com>
2023-11-01 17:07:56 +0530

    [1.1] Products > Checking the functionality of getProductsById()

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,13 @@
+import { getProductsById } from "@/lib/actions";
+import React from "react";
+
+const Product = async ({ params }: { params: { id: String } }) => {
+    try {
+        const product = await getProductsById(params.id);
+        return <div>{product.title}</div>;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default Product;
