05a12684403f593fc54906841c0c65111c898036
Ashu0Singh <ashusingh1417@gmail.com>
2023-11-10 18:30:13 +0530

    [1.1] Emoji Update

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -5,11 +5,11 @@ let isConnected = false;
 export const connectToDb = async () => {
 	mongoose.set("strictQuery", true);
     if (!process.env.MONGO_URI) return console.log("Mongo_URI not defined");
-    if (isConnected) return console.log("=> Using existing database connection");
+    if (isConnected) return console.log(" 😏 Using existing database connection");
     try {
         await mongoose.connect(process.env.MONGO_URI);
         isConnected = true;
-        console.log("=> Database Connected");
+        console.log(" 😒 Database Connected");
     } catch (error) {
         console.log(error);
     }
